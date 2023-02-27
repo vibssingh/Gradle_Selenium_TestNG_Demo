dbe056ccf18cac6517de77b218bbba7273d9c615
Vibha Singh <vibhasingh2004@gmail.com>
2023-02-27 23:15:19 +0000

    Headless browser

--- src/test/java/org/example/tests/BaseTests.java
+++ src/test/java/org/example/tests/BaseTests.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -19,7 +20,11 @@ public class BaseTests {
     @Step("Start the application")
     public void setup() {
     	WebDriverManager.chromedriver().setup();
-	    driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
 	    driver.manage().window().maximize();
 	    driver.get("https://opensource-demo.orangehrmlive.com/");	    
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
